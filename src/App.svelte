f055594415b05ec408fe0cc0592cc5599c96a5c3
Umar Younis <mumar.younisa@gmail.com>
2021-02-26 09:58:31 -0600

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { Router, Link, Route } from "svelte-routing";
+	import AppRouter from "./Router.svelte";
+</script>
+
+<style>
+
+</style>
+
+<div>
+	<Router>
+		<Link to="/">Home</Link>
+		<Link to="/todo">Todo</Link>
+	</Router>
+	<AppRouter/>
+</div>
---
+++ src/input.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { onMount } from 'svelte';
+	let todoText = "";
+	let inputEle;
+	export let onAdd;
+	let onClickAdd = (e) => {
+		onAdd(todoText);
+		todoText = "";
+		return false;
+	}
+
+	onMount(() => {
+    inputEle.focus();
+  });
+</script>
+
+<style>
+input {
+	width: 90%;
+}
+div {
+	margin-top: 15px;
+}
+</style>
+
+<div>
+	<form on:submit|preventDefault={onClickAdd}>
+		<input bind:this={inputEle} bind:value={todoText} type="text"/>
+		<button>Add</button>
+	</form>
+</div>
